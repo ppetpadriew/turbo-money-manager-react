4c9cd1dcf89d19ac3c8fc976f814d63e5740040b
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2017-08-17 21:38:54 +0700

    chore(Webpack config)
    - Explicitly define port for webpack dev server.
    - Add loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var webpack = require("webpack");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var port = 8080;
 var config = {
     resolve: {
         extensions: [".ts", ".tsx", ".js", ".json"]
@@ -10,7 +12,7 @@ var config = {
 
         // bundle the client for webpack-dev-server
         // and connect to the provided endpoint
-        "webpack-dev-server/client?http://localhost:8080",
+        "webpack-dev-server/client?http://localhost:" + port,
 
         // bundle the client for hot reloading
         // only- means to only hot reload for successful updates
@@ -18,11 +20,53 @@ var config = {
 
         "./src/index.tsx"
     ],
+    // Enable source-map for debugging webpack's output.
+    devtool: "source-map",
     module: {
-        rules: [
+        loaders: [
             {
                 test: /\.tsx?$/,
                 loaders: ["awesome-typescript-loader"]
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback:'style-loader',
+                    use: ['css-loader', 'sass-loader'],
+                    publicPath: '/assets/'
+                })
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader',
+                    publicPath: '/assets/'
+                })
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
             }
         ]
     },
@@ -43,9 +87,14 @@ var config = {
 
         // do not emit compiled assets that include errors
         new webpack.NoEmitOnErrorsPlugin(),
+
+        new ExtractTextPlugin({
+            filename: 'style.css',
+            allChunks: true
+        })
     ],
     devServer: {
-
+        port: port,
         // enable HMR on the server
         hot: true
     },
